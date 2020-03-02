f44bce2af425dbb1e6bb0d26eee043b780e7743e
0-0 <394661930@qq.com>
2020-03-02 18:07:38 +0800

    依赖问题解决

---
+++ ftd-feign/src/main/java/com/xs/middle/feign/demo/FtdFeignApplication.java
@@ -0,0 +1,13 @@
+package com.xs.middle.feign.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtdFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtdFeignApplication.class, args);
+    }
+
+}
